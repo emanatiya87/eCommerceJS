828ffb358d2e68b81cd1f4efc2bc7916a998e083
Eman <emanatiya87@gmail.com>
2025-04-11 03:51:03 +0200

    total price work

--- js/cartScript.js
+++ js/cartScript.js
@@ -1,4 +1,5 @@
 let cartBody = document.getElementById("cartBody");
+let addedToCart = JSON.parse(sessionStorage.getItem("cartProduct")); // parse string to array
 function displayInCart() {
   let addedToCart = JSON.parse(sessionStorage.getItem("cartProduct")); // parse string to array
   if (sessionStorage.getItem("cartProduct") && addedToCart.length != 0) {
@@ -16,8 +17,8 @@ function displayInCart() {
           </td>
           <td>${item[2]}</td>
           <td>${item[0]}</td>
-          <td><input type="number" min="1" value="2" /></td>
-          <td>${item[0] * 2}</td>
+          <td><input type="number" min="1" class="quantity" value="1"/></td>
+          <td class="totalPrice"></td>
         </tr>
       `;
     });
@@ -34,3 +35,17 @@ function removeRow(index) {
   sessionStorage.setItem("cartProduct", JSON.stringify(cartProduct));
   displayInCart();
 }
+let quantity = document.querySelectorAll(".quantity");
+let totalPrice = document.querySelectorAll(".totalPrice");
+function updatePrice(quantity, index) {
+  let eachquantity = quantity.value;
+  let totalPriceValue = parseInt(eachquantity) * Number(addedToCart[index][0]);
+  totalPrice[index].innerText = totalPriceValue;
+  totalPrice[index].value = totalPriceValue;
+}
+quantity.forEach((quantity, index) => {
+  updatePrice(quantity, index);
+  quantity.onchange = () => {
+    updatePrice(quantity, index);
+  };
+});
