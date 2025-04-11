a0bae2ba380285068149eae971027656e925a382
Eman <emanatiya87@gmail.com>
2025-04-11 05:00:38 +0200

    cart calculations

--- js/cartScript.js
+++ js/cartScript.js
@@ -36,16 +36,52 @@ function removeRow(index) {
   displayInCart();
 }
 let quantity = document.querySelectorAll(".quantity");
-let totalPrice = document.querySelectorAll(".totalPrice");
+let totalPriceForItem = document.querySelectorAll(".totalPrice");
 function updatePrice(quantity, index) {
   let eachquantity = quantity.value;
-  let totalPriceValue = parseInt(eachquantity) * Number(addedToCart[index][0]);
-  totalPrice[index].innerText = totalPriceValue;
-  totalPrice[index].value = totalPriceValue;
+  let totalPriceValueForItem =
+    parseInt(eachquantity) * Number(addedToCart[index][0]);
+  totalPriceForItem[index].innerText = totalPriceValueForItem;
+  totalPriceForItem[index].value = totalPriceValueForItem;
 }
 quantity.forEach((quantity, index) => {
   updatePrice(quantity, index);
   quantity.onchange = () => {
     updatePrice(quantity, index);
+    updateSubTotal();
+    updateTotalPrice();
   };
 });
+let CartSubTotal = document.getElementById("CartSubTotal");
+let CouponsDiscount = document.getElementById("CouponsDiscount");
+let Total = document.getElementById("Total");
+
+function updateSubTotal() {
+  let totalPrice = 0;
+  totalPriceForItem.forEach((price) => {
+    totalPrice += Number(price.value);
+    CartSubTotal.innerText = totalPrice;
+    CartSubTotal.value = totalPrice;
+  });
+}
+let notValidCoupon = document.getElementById("notValidCoupon");
+updateSubTotal();
+let enterCoupon = document.getElementById("enterCoupon");
+let applayCoupon = document.getElementById("applayCoupon");
+applayCoupon.onclick = function () {
+  if (enterCoupon.value == "Eman") {
+    CouponsDiscount.value = 50;
+    CouponsDiscount.innerText = 50;
+    notValidCoupon.style.display = "none";
+  } else {
+    CouponsDiscount.value = 0;
+    CouponsDiscount.innerText = 0;
+    notValidCoupon.style.display = "block";
+  }
+  updateTotalPrice();
+};
+function updateTotalPrice() {
+  Total.innerText =
+    Number(CartSubTotal.innerText) + Number(CouponsDiscount.innerText);
+}
+updateTotalPrice();
