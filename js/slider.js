14fdec9b2c5d993132c9ca7645d0e600cafffb88
Eman <emanatiya87@gmail.com>
2025-04-14 04:14:39 +0200

    slider

--- js/slider.js
+++ js/slider.js
@@ -6,5 +6,24 @@ document.addEventListener("DOMContentLoaded", function () {
     focus: "center",
     pagination: false,
     gap: "1rem",
+    breakpoints: {
+      768: {
+        perPage: 1,
+        gap: "1rem",
+      },
+    },
+  }).mount();
+  new Splide("#my-splide-2", {
+    perPage: 3,
+    perMove: 1,
+    focus: "center",
+    pagination: false,
+    gap: "1rem",
+    breakpoints: {
+      768: {
+        perPage: 1,
+        gap: "1rem",
+      },
+    },
   }).mount();
 });
